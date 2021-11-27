a50512be39771cea7425687781c4badc3e5173e8
lzlocki <lzlocki@poczta.fm>
2021-11-27 09:26:40 +0100

    Add reading from file and transfering data to list ov int values. Code cleandup

--- main.py
+++ main.py
@@ -9,29 +9,25 @@ from Raport import Raport
 from RaportGenerator import RaportGenerator
 
 
-
 # Todos :
-# Pobrac dane z pliku
-
-# done - Dodac obliczenia na procesach, ktorych liczba zalezy od liczby dostepnych procesorow w systemie
-# done - Pobawic sie raport generatorem i wstepnie wygenerowac raport podmieniajac wartosci 
-# done - Dodac obliczanie na cztery procesy
 
 
-# Glowne zadanie !!
-
-"""
-Values = [15972490, 80247910, 92031257, 75940266,
-            97986012, 87599664, 75231321, 11138524,
-            68870499, 11872796, 79132533, 40649382,
-            63886074, 53146293, 36914087, 62770938]
-"""
-
-
-Values = [10, 20, 30, 40,
-            50, 60, 70, 80,
-            90, 100, 110, 120,
-            130, 140, 150, 160]
+# returns list of values
+def readingValuesFromFile(fileName):
+    values = 0
+    with open(fileName, "r") as file:
+        data = file.readlines()
+        # removing '\n' from list of strings
+        String = [x.strip()for x in data ]
+        # Creating string with values
+        valuesString = ""
+        for element in String:
+            valuesString += element
+        valuesString = valuesString.replace(" ", "")
+        valueStringSplitted = valuesString.split(",")
+        # Converting stringValues to int list
+        values = [int(x) for x in valueStringSplitted]
+    return values
 
 
 # Main calculation  
@@ -60,8 +56,14 @@ def performMultithreadingTest(values, threads):
         executor.map(calculation, values)
 
 
+# Main function
 def main():
 
+    Values = readingValuesFromFile("task2.txt") 
+    print(Values)
+    
+    c = input("spacja kontynuacja")
+    
     raport_1xMultiThreading = Raport()
     raport_4xMultiThreading = Raport()
     raport_4xMultiProcessing = Raport()
@@ -133,111 +135,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-    # !! SYMULACJA TESTOW I ZBIERANIA DANYCH DO RAPORTOW !!
-    """"
-    # 1x thread
-    Raport2 = Raport()
-    Raport2.setTestDescription("1 thread")
-    Raport2.setSystemInterpreterAndProcessorInfo()
-
-    Raport2.addProbe(35.5411)
-    Raport2.addProbe(33.5422)
-    Raport2.addProbe(37.5433)
-    Raport2.addProbe(33.5444)
-    Raport2.addProbe(35.5455)
-    
-    # 4x threads
-    Raport1 = Raport()
-    Raport1.setTestDescription("4 threads")
-    Raport1.setSystemInterpreterAndProcessorInfo()
-
-    Raport1.addProbe(25.5411)
-    Raport1.addProbe(23.5422)
-    Raport1.addProbe(27.5433)
-    Raport1.addProbe(23.5444)
-    Raport1.addProbe(25.5455)
-
-    # 4x processes
-    Raport3 = Raport()
-    Raport3.setTestDescription("4 processes")
-    Raport3.setSystemInterpreterAndProcessorInfo()
-
-    Raport3.addProbe(15.5411)
-    Raport3.addProbe(13.5422)
-    Raport3.addProbe(17.5433)
-    Raport3.addProbe(13.5444)
-    Raport3.addProbe(15.5455)
-
-    # Dodanie raportow
-    ListaRaportow.append(Raport1)
-    ListaRaportow.append(Raport2)
-    ListaRaportow.append(Raport3)
-
-
-    a = input("Stop ! Rapoty zebrane wcisnij enter by wydrukowac dane! ")
-
-    # Wydruki raportow : 
-    print("Dostepne raporty : ")
-    for e in ListaRaportow:
-        print(" ", e.getTestDescription())
-        print(" ", e.getMedianOfAllProbes())
-
-    a = input("Stop ! Przejscie do generowania raportu! ")
-
-    # Generator raportu - testy
-    Generator = RaportGenerator(ListaRaportow)
-    htmlPart = Generator.showHtml()
-    print(htmlPart)
-
-    a = input("Stop ! Przejscie do save raportu do HTML! ")
-
-    # SAVE raportow
-
-    Generator.saveHtml()
-
-    #print(timeit.timeit(funkcja(Values), 1))
-
-    """
-
-
-    """
-    # 1x multithreading 
-    print("Test 1x multithreading")
-    start = timeit.default_timer()
-    performMultithreadingTest(Values, 1)
-    end = timeit.default_timer()
-    result = end - start
-    print(f"1x multithreading: {round(result, 3)} sekund")
-    raport_1xMultiThreading.addProbe(result) # adding result to raport
-
-    # 4x multithreating 
-    print("Test 4x multithreating")
-    start = timeit.default_timer()
-    performMultithreadingTest(Values, 4)
-    end = timeit.default_timer()
-    result = end - start
-    print(f"4x multithreating: {round(result, 3)} sekund")
-    raport_4xMultiThreading.addProbe(result) # adding result to raport
-
-    # 4x multiprocessing 
-    print("Test 4x multiprocess")
-    start = timeit.default_timer()
-    performMultiprocessTest(Values, 4)
-    end = timeit.default_timer()
-    result = end - start
-    print(f"4x multiprocess: {round(result, 3)} sekund")
-    raport_4xMultiProcessing.addProbe(result) # adding result to raport
-
-    # multiprocessing according to CPUs
-    cpus = raport_CpusMultiProcessing.getCpusNumber()
-    print("Test multiprocess according to number of CPUs: ")
-    for _ in range(5):
-        start = timeit.default_timer()
-        performMultiprocessTest(Values, cpus)
-        end = timeit.default_timer()
-        result = end - start
-        print(f"multiprocess by {cpus} CPUs: {round(result, 3)} sekund")
-        raport_CpusMultiProcessing.addProbe(result) # adding result to raport
-    """
